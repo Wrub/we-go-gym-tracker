7926a1c222b94b6939726a59824264dc7bdccf4f
Wrub <viniciuswrub01@gmail.com>
2025-10-28 10:15:34 -0300

    fix: listExercises route

--- api/src/main/routes/exercise.route.ts
+++ api/src/main/routes/exercise.route.ts
@@ -3,6 +3,6 @@ import { adaptRoute } from "main/adapters/adapt-route";
 import { makeCreateExerciseController, makeListExercisesController } from "main/factories/exercise";
 
 export default (router: Router) => {
-  router.get("/exercises", adaptRoute(makeListExercisesController()));
+  router.get("/exercise", adaptRoute(makeListExercisesController()));
   router.post("/exercise", adaptRoute(makeCreateExerciseController()));
 };
